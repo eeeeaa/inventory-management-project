fadf083ec685d5f9b6dde6ace3ad8b56e10eda8d
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-03-19 19:04:26 +0700

    refactor item model

--- models/item.js
+++ models/item.js
@@ -5,9 +5,15 @@ const Schema = mongoose.Schema;
 const ItemSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   description: { type: String, required: true },
-  category_id: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-  price: { type: Number, min: 0 },
-  quantity: { type: Number, min: 0 },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, min: 0, required: true },
+  quantity: {
+    type: Number,
+    min: 0,
+    required: true,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+  },
 });
 
 ItemSchema.virtual("url").get(function () {
